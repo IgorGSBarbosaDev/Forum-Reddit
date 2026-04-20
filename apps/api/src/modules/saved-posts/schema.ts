3fbd5fd4fc839307a47e6ac7308055013bcbbbc6
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-04-20 09:00:36 -0300

    refactor(api-modules): remove implementacoes legadas migradas para packages

--- apps/api/src/modules/saved-posts/schema.ts
+++
@@ -1,5 +0,0 @@
-import { postIdParamsSchema } from "../../schemas/common/id.schema";
-import { paginationQuerySchema } from "../../schemas/common/pagination.schema";
-
-export const savedPostParamsSchema = postIdParamsSchema;
-export const listSavedPostsQuerySchema = paginationQuerySchema;
